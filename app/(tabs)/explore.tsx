dde54d0e2fd298905bc58c4e41a6ae56fc13ea55
JosephMugo8711 <wamiti8711@gmail.com>
2024-05-16 16:04:19 +0300

    first commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -24,7 +24,6 @@ export default function TabTwoScreen() {
         </ThemedText>
         <ThemedText>
           The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
         </ThemedText>
         <ExternalLink href="https://docs.expo.dev/router/introduction">
           <ThemedText type="link">Learn more</ThemedText>
